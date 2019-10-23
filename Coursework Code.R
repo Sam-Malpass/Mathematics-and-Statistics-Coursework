8ae262a6e03177a9564027518933991cf1b51027
Unknown <sam@malpass.me.uk>
2019-10-23 14:37:45 +0100

    Write Question 3 R Code

--- Coursework Code.R
+++ Coursework Code.R
@@ -1,3 +1,13 @@
 # Mathematics and Statistics Coursework
 # By Samuel John Malpass
 
+# Question 1
+# Write R code to reproduce a graph
+
+# Question 2
+
+# Question 3
+# Calculate the singular values and matrices in the singular value decomposition
+A<-cbind(c(1,1,1,1),c(2,0,0,0)) # Declare matrix A
+sv<-svd(A)
+sv
